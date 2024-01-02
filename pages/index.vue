967abdcf7d7fd1316b8de687a0d0e80148ba6588
ITB-karle <karle@itbrightsolution.com>
2024-01-02 23:01:13 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
         class="mySwiper"
       >
         <swiper-slide>
-          <img src="../assets/images/Baanthai_home.webp" alt="" class="w-full max-h-[75vh]">
+          <img src="../assets/images/HomeBanner_1.webp" alt="" class="w-full">
           <div class="swiper-button text-xs md:text-xl lg:text-3xl font-medium text-white text-center mx-auto">
             <!-- <a href="https://weborder.zeoniq.com/one/init-b">
               <button class="block bg-lightgold py-1 lg:py-2 px-3 lg:px-8 rounded-full mb-2 lg:mb-8 shadow-lg">Order Now</button>
@@ -28,7 +28,18 @@
           </div>
         </swiper-slide>
         <swiper-slide>
-          <img src="../assets/images/CNY_Banner.webp" alt="" class="w-full max-h-[75vh]">
+          <img src="../assets/images/HomeBanner_2.webp" alt="" class="w-full">
+          <div class="swiper-button text-xs md:text-xl lg:text-3xl font-medium text-white text-center mx-auto">
+            <!-- <a href="https://weborder.zeoniq.com/one/init-b">
+              <button class="block bg-lightgold py-1 lg:py-2 px-3 lg:px-8 rounded-full mb-2 lg:mb-8 shadow-lg">Order Now</button>
+            </a>
+            <a href="https://www.tableapp.com/baan-thai-2-seafood-thai-cuisine?utm_source=Official+Website&utm_campaign=Tableapp&utm_medium=Tableapp">
+              <button class="block bg-lightgold py-1 lg:py-2 px-3 lg:px-8 rounded-full mb-2 lg:mb-8 shadow-lg">VIP Reservation</button>
+            </a> -->
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/images/CNY_Banner.webp" alt="" class="w-full">
           <div class="swiper-button text-xs md:text-xl lg:text-3xl font-medium text-white text-center mx-auto">
             <!-- <a href="https://weborder.zeoniq.com/one/init-b">
               <button class="block bg-lightgold py-1 lg:py-2 px-3 lg:px-8 rounded-full mb-2 lg:mb-8 shadow-lg">Order Now</button>
